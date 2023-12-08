32b5db3eed1673694ad349d8164de25c94850644
Adele130 <adele.edouard@epfedu.fr>
2023-12-08 08:05:28 +0100

    Première MAquette Hamburger

--- JS/script.js
+++ JS/script.js
@@ -25,28 +25,11 @@ showSlide(currentIndex);
 document.querySelector('.nextBtn').addEventListener('click', nextSlide);
 document.querySelector('.prevBtn').addEventListener('click', previousSlide);
 
-//Menu Toggle
-//function toggleMenu() {
-    //var navigation = document.querySelector('.menu .Navigation'); // Correction ici
-    //navigation.classList.toggle('show-menu');
-//}
-
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon
-function myFunction() {
-    var menu = document.querySelector(".menu");
-    if (menu.classList.contains("responsive")) {
-        menu.classList.remove("responsive");
+function toggleMenu() {
+    var x = document.getElementById("idmenu");
+    if (x.className === "menu") {
+        x.className += " responsive";
     } else {
-        menu.classList.add("responsive");
+        x.className = "menu";
     }
 }
-
-document.querySelector('.menu-icon').addEventListener('click', function () {
-    document.querySelector('.menu').classList.toggle('show');
-});*/
-function toggleMenu() {
-    var navigation = document.querySelector('.Navigation');
-    navigation.classList.toggle('show');
-}
-
-
